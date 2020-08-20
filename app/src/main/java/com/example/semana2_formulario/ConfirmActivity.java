5733cfc2845d9565ad81cadd16a505875eb61000
jose <josealsola29@gmail.com>
2020-08-20 16:51:37 -0500

    Reformat Code

--- app/src/main/java/com/example/semana2_formulario/ConfirmActivity.java
+++ app/src/main/java/com/example/semana2_formulario/ConfirmActivity.java
@@ -1,44 +1,69 @@
 package com.example.semana2_formulario;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
 public class ConfirmActivity extends AppCompatActivity {
+    private TextView tvNameInfo;
+    private TextView tvBirthDate;
+    private TextView tvTelephoneInfo;
+    private TextView tvEmailInfo;
+    private TextView tvDescriptionInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
         Bundle extras = getIntent().getExtras();
-        String name = extras.getString("name");
+        String name = Objects.requireNonNull(extras).getString("name");
         String tellphone = extras.getString("tellphone");
         String email = extras.getString("email");
         String description = extras.getString("description");
-        String date = String.valueOf(extras.getLong("date"));
+        String date = extras.getString("date");
 
-        TextView tvNameInfo = findViewById(R.id.tvNameInfo);
-        TextView tvBirthDate = findViewById(R.id.tvBirthDate);
-        TextView tvTelephoneInfo = findViewById(R.id.tvTelephoneInfo);
-        TextView tvDescriptionInfo = findViewById(R.id.tvDescriptionInfo);
+        tvNameInfo = findViewById(R.id.tvNameInfo);
+        tvBirthDate = findViewById(R.id.tvBirthDate);
+        tvTelephoneInfo = findViewById(R.id.tvTelephoneInfo);
+        tvEmailInfo = findViewById(R.id.tvEmailInfo);
+        tvDescriptionInfo = findViewById(R.id.tvDescriptionInfo);
+        Button btnEditInfo = findViewById(R.id.btnEditInfo);
 
         tvNameInfo.setText(name);
         tvBirthDate.setText(date);
         tvTelephoneInfo.setText(tellphone);
+        tvEmailInfo.setText(email);
         tvDescriptionInfo.setText(description);
 
+        btnEditInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfirmActivity.this, MainActivity.class);
+                intent.putExtra("name", tvNameInfo.getText().toString());
+                intent.putExtra("birthday", tvBirthDate.getText().toString());
+                intent.putExtra("tellphone", tvTelephoneInfo.getText().toString());
+                intent.putExtra("email", tvEmailInfo.getText().toString());
+                intent.putExtra("description", tvDescriptionInfo.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 
 
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        return super.onKeyDown(keyCode, event);
-//        if (keyCode == KeyEvent.KEYCODE_BACK){
-//            Intent intent = new Intent(ConfirmActivity.this, MainActivity.class);
-//            startActivity(intent);}
-//    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent intent = new Intent(ConfirmActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
 }
--- app/src/main/java/com/example/semana2_formulario/MainActivity.java
+++ app/src/main/java/com/example/semana2_formulario/MainActivity.java
@@ -2,21 +2,22 @@ package com.example.semana2_formulario;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
+import android.widget.DatePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private TextInputEditText tilName;
     private TextInputEditText tilTelephone;
     private TextInputEditText tilEmail;
     private TextInputEditText tilDescription;
-    private CalendarView calendarView;
+    private DatePicker datePicker;
     private Button btnNext;
 
     @Override
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         tilTelephone = findViewById(R.id.tilTelephone);
         tilEmail = findViewById(R.id.tilEmail);
         tilDescription = findViewById(R.id.tilDescription);
-        calendarView = findViewById(R.id.calendarView);
+        datePicker = findViewById(R.id.datePicker);
         btnNext = findViewById(R.id.btnNext);
 
         btnNext.setOnClickListener(new View.OnClickListener() {
@@ -39,13 +40,26 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("tellphone", tilTelephone.getText().toString());
                 intent.putExtra("email", tilEmail.getText().toString());
                 intent.putExtra("description", tilDescription.getText().toString());
-                intent.putExtra("date", calendarView.getDate());
+                String dateFormat = datePicker.getDayOfMonth() +"/"
+                        +datePicker.getMonth()+"/"
+                        +datePicker.getYear();
+                intent.putExtra("date", dateFormat);
                 startActivity(intent);
-
-
             }
         });
 
+        Bundle extras = getIntent().getExtras();
+        if(extras !=null){
 
+        String name = Objects.requireNonNull(extras).getString("name");
+        String tellphone = extras.getString("tellphone");
+        String email = extras.getString("email");
+        String description = extras.getString("description");
+        String date = extras.getString("date");
+            tilName.setText(name);
+            tilTelephone.setText(tellphone);
+            tilEmail.setText(email);
+            tilDescription.setText(description);
+        }
     }
 }
